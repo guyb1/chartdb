08bb9e0c32891555a23ae71af6adbedf9d4402a0
Guy Ben-Aharon <guybenah@gmail.com>
2024-08-31 12:41:31 +0300

    update

--- src/components/code-snippet/code-snippet.tsx
+++ src/components/code-snippet/code-snippet.tsx
@@ -1,5 +1,5 @@
-import { cn } from '@/lib/utils';
 import React from 'react';
+import { cn } from '@/lib/utils';
 import { CopyBlock, atomOneDark } from 'react-code-blocks';
 import { CodeBlockProps } from 'react-code-blocks/dist/components/CodeBlock';
 
